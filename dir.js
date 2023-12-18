43493f62f7f4692412868668e769ad0c951a6339
backup1122 <142592568+backup1122@users.noreply.github.com>
2023-12-19 01:30:46 +0600

    Update file

--- dir.js
+++ dir.js
@@ -1,4 +1,4 @@
 k='https://raw.githubusercontent.com/backup1122/galleryfiles/master/';
-DATAf=[k+"0", k+"8", k+"9"];
-DATAd=[k+"10", k+"11", k+"12", k+"13", k+"14", k+"15", k+"16", k+"17", k+"18", k+"19"];
+DATAf=[k+"0", k+"1", k+"2", k+"8", k+"9"];
+DATAd=[k+"10", k+"13", k+"14", k+"15", k+"16", k+"17", k+"18", k+"19"];
 DATA=DATAd.concat(DATAf);
